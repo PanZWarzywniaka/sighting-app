9c334b589308c0ab89f3c6f551f6f41cf52527f1
acb19sh <shassan6@sheffield.ac.uk>
2023-03-13 02:28:17 +0000

    added comments

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -4,7 +4,7 @@ let marker;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'),{disableDefaultUI: true});
     map.setZoom(15);
-    // Get current users location, canter map to sheffield if error or user doesn't allow access to location
+    // Get current users' location, center map to sheffield if error or user doesn't allow access to location
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             let initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
